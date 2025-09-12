ee8f6a68593f9309573fa917cb4f96c688d2b321
abhiramkmarar <77226184+abhiramkmarar@users.noreply.github.com>
2025-09-12 23:52:34 +0530

    Update index.php

--- Website/index.php
+++ Website/index.php
@@ -13,19 +13,14 @@ session_start();
 <p>
 <?php
 
-if (!isset($_SESSION['user_id'])) {
-    echo "<a href='login.php'>Login</a> | <a href='register.php'>Register</a>";
-} else {
+echo "Welcome, " . $_SESSION['username'] . "! ";
+echo "<a href='profile_page.php'>Profile</a> | <a href='logout.php'>Logout</a>";
 
-    echo "Welcome, " . $_SESSION['username'] . "! ";
-    echo "<a href='profile_page.php'>Profile</a> | <a href='logout.php'>Logout</a>";
-'''yet to create a profile_page.php '''
-    
-    if ($_SESSION['role'] == 'admin') {
-        echo " | <a href='admin.php'>Admin Dashboard</a>";
-    '''yet to make admin page'''
-    }
+if ($_SESSION['role'] == 'admin') {
+    echo " | <a href='admin.php'>Admin Dashboard</a>";
+    // yet to make admin page
 }
+
 ?>
 </p>
 
