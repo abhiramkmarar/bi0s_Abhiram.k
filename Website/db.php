327b2beacd075206edd2ce2c2c7caf6d26f7c1f5
abhiramkmarar <77226184+abhiramkmarar@users.noreply.github.com>
2025-11-04 20:28:56 +0530

    Update database credentials in db.php

--- Website/db.php
+++ Website/db.php
@@ -1,12 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "user_system";
+$username   = "phpuser";
+$password   = "PhpUser@12345"; 
+$dbname     = "user_system";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 ?>
+
